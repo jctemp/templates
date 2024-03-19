e4f9cc33227f8c4ca463f90d82cc57fbc009f7b3
Jamie Temple <jamie-temple@live.de>
2024-03-19 14:50:44 +0100

    feat(python-ml): environment for machine learning

---
+++ python-ml/flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Python ML Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+          overlays = [
+            (final: prev: {
+              pythonPackagesExtensions = [
+                (final: prev: {
+                  torch = final.torch-bin;
+                  torchvision = final.torchvision-bin;
+                })
+              ];
+            })
+          ];
+        };
+      in {
+        formatter = pkgs.alejandra;
+        devShell = pkgs.mkShell {
+          name = "machine-learning";
+          packages =
+            (with pkgs; [
+              alejandra
+              python311
+              nodejs
+            ])
+            ++ (
+              if pkgs.config.cudaSupport
+              then
+                (with pkgs.cudaPackages; [
+                  cudatoolkit
+                  cudnn
+                ])
+              else []
+            )
+            ++ (with pkgs.python311Packages; [
+              # Generic
+              jupyter
+              ipykernel
+              black
+              pip
+
+              # DataScience
+              numpy
+              pandas
+              polars
+              matplotlib
+              seaborn
+              scikit-learn
+
+              # PyTorch
+              torch
+              torchvision
+              torchinfo
+            ]);
+        };
+      }
+    );
+}
